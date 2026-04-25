77ee41aa261762d243af0e370d727781ac9a9317
Ayoub <ayoubbenbarkat1342@gmail.com>
2026-04-25 22:15:57 +0100

    init

---
+++ build/CMakeFiles/IoTDashboard.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/media/ayoub/New Volume/Projects/IoTDashboard/src/DashboardUI.cpp" "CMakeFiles/IoTDashboard.dir/src/DashboardUI.cpp.o" "gcc" "CMakeFiles/IoTDashboard.dir/src/DashboardUI.cpp.o.d"
+  "/media/ayoub/New Volume/Projects/IoTDashboard/src/Logger.cpp" "CMakeFiles/IoTDashboard.dir/src/Logger.cpp.o" "gcc" "CMakeFiles/IoTDashboard.dir/src/Logger.cpp.o.d"
+  "/media/ayoub/New Volume/Projects/IoTDashboard/src/Parser.cpp" "CMakeFiles/IoTDashboard.dir/src/Parser.cpp.o" "gcc" "CMakeFiles/IoTDashboard.dir/src/Parser.cpp.o.d"
+  "/media/ayoub/New Volume/Projects/IoTDashboard/src/SensorSimulator.cpp" "CMakeFiles/IoTDashboard.dir/src/SensorSimulator.cpp.o" "gcc" "CMakeFiles/IoTDashboard.dir/src/SensorSimulator.cpp.o.d"
+  "/media/ayoub/New Volume/Projects/IoTDashboard/src/main.cpp" "CMakeFiles/IoTDashboard.dir/src/main.cpp.o" "gcc" "CMakeFiles/IoTDashboard.dir/src/main.cpp.o.d"
+  "" "IoTDashboard" "gcc" "CMakeFiles/IoTDashboard.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/IoTDashboard.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IoTDashboard.dir/link.d"
+  "CMakeFiles/IoTDashboard.dir/src/DashboardUI.cpp.o"
+  "CMakeFiles/IoTDashboard.dir/src/DashboardUI.cpp.o.d"
+  "CMakeFiles/IoTDashboard.dir/src/Logger.cpp.o"
+  "CMakeFiles/IoTDashboard.dir/src/Logger.cpp.o.d"
+  "CMakeFiles/IoTDashboard.dir/src/Parser.cpp.o"
+  "CMakeFiles/IoTDashboard.dir/src/Parser.cpp.o.d"
+  "CMakeFiles/IoTDashboard.dir/src/SensorSimulator.cpp.o"
+  "CMakeFiles/IoTDashboard.dir/src/SensorSimulator.cpp.o.d"
+  "CMakeFiles/IoTDashboard.dir/src/main.cpp.o"
+  "CMakeFiles/IoTDashboard.dir/src/main.cpp.o.d"
+  "IoTDashboard"
+  "IoTDashboard.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/IoTDashboard.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
